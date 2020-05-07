2261cddb7b09db9609453ec87f63a1336d0fb32f
Beaxadity <43746658+Beaxadity@users.noreply.github.com>
2020-05-07 04:23:47 +0000

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Þetta er þar sem maður fær player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Færi bílinn áfram
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Snýr bílnum
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
